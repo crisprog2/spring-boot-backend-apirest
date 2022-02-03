4716ba287df448666ae59b47ce0c6de0522ba5c8
christian chacon <christian_kazuma@hotmail.com>
2022-02-03 09:59:34 -0500

    Se crean los servicios para la tabla Cliente

---
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/service/ClienteServiceImpl.java
@@ -0,0 +1,24 @@
+package com.bolsadeideas.springboot.backend.apirest.models.service;
+
+import java.util.List;
+
+import com.bolsadeideas.springboot.backend.apirest.models.dao.IClienteDao;
+import com.bolsadeideas.springboot.backend.apirest.models.entities.Cliente;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ClienteServiceImpl implements IClienteService {
+    
+    @Autowired
+    private IClienteDao clienteDao;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Cliente> findAll() {
+        return (List<Cliente>) clienteDao.findAll();
+    }
+    
+}
