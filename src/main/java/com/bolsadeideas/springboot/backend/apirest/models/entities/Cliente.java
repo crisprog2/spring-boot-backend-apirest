176d78f3d23cc234ab2d8640c77f51bb9ec2279a
christian chacon <christian_kazuma@hotmail.com>
2022-02-03 12:27:02 -0500

    Se implementan los servicios en el controlador de Cliente

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entities/Cliente.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entities/Cliente.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -25,6 +26,11 @@ public class Cliente implements Serializable{
     @Temporal(TemporalType.DATE)
     private Date createAt;
     private String email;
+
+    @PrePersist
+    public void prePersist(){
+        createAt =  new Date();
+    }
 
     public Long getId() {
         return id;
