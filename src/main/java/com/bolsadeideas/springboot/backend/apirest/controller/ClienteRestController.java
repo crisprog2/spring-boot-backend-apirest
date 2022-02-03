5a0356dc8520e2ea47892acc0a1a9d4c4459a0eb
christian chacon <christian_kazuma@hotmail.com>
2022-02-03 17:09:56 -0500

    Se modifica el error del show cuando no hay acceso a la bd con un error 500

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/controller/ClienteRestController.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/controller/ClienteRestController.java
@@ -47,7 +47,7 @@ public class ClienteRestController {
         } catch (DataAccessException e) {
             response.put("mensaje", "Error al realizar la consulta en la base de datos");
             response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
-            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
         }
         if(cliente==null){
             response.put("mensaje", "El cliente ID:".concat(id.toString().concat(" no existe en la base de datos!")));
