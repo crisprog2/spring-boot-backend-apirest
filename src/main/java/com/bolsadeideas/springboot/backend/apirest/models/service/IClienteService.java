20c0a2ae2e7b7ccc0e4b63a35c3fffcbcc488f49
christian chacon <christian_kazuma@hotmail.com>
2022-02-04 11:10:52 -0500

    Se crea la implementacion del paginador en el servicio y en el controlador

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/models/service/IClienteService.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/service/IClienteService.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import com.bolsadeideas.springboot.backend.apirest.models.entities.Cliente;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface IClienteService {
     public List<Cliente> findAll();
+    public Page<Cliente> findAll(Pageable pageable);
     public Cliente findById(Long id);
     public Cliente save(Cliente cliente);
     public void delete(Long id);
