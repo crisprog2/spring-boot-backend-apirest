b9e7ad92f6aca23469fef33e3d4247ee543401c3
christian chacon <christian_kazuma@hotmail.com>
2022-02-04 09:57:35 -0500

    Se retiran los imports que no se están usando

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/controller/ClienteRestController.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/controller/ClienteRestController.java
@@ -1,6 +1,5 @@
 package com.bolsadeideas.springboot.backend.apirest.controller;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,7 +15,6 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
