0ab016b4dc4c049f0bd656a8a35b64e758429fc3
christian chacon <christian_kazuma@hotmail.com>
2022-02-09 10:24:16 -0500

    Creando nueva clase entity Region

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/models/dao/IClienteDao.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/dao/IClienteDao.java
@@ -1,9 +1,15 @@
 package com.bolsadeideas.springboot.backend.apirest.models.dao;
 
+import java.util.List;
+
 import com.bolsadeideas.springboot.backend.apirest.models.entities.Cliente;
+import com.bolsadeideas.springboot.backend.apirest.models.entities.Region;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface IClienteDao extends JpaRepository<Cliente, Long> {
-    
+
+    @Query("from Region")
+    public List<Region> findAllRegiones();
 }
--- src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entities/Cliente.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entities/Cliente.java
@@ -5,9 +5,12 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -16,9 +19,11 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
 @Table(name = "clientes")
-public class Cliente implements Serializable{
+public class Cliente implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -44,41 +49,65 @@ public class Cliente implements Serializable{
 
     private String foto;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id")
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    private Region region;
+
     public String getFoto() {
         return foto;
     }
+
     public void setFoto(String foto) {
         this.foto = foto;
     }
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     public String getApellido() {
         return apellido;
     }
+
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
+
     public Date getCreateAt() {
         return createAt;
     }
+
     public void setCreateAt(Date createAt) {
         this.createAt = createAt;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
-    
+
+    public Region getRegion() {
+        return region;
+    }
+
+    public void setRegion(Region region) {
+        this.region = region;
+    }
+
 }
