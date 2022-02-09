328a5b46adbd98f0742e8ff9660b2bf2fedd3bb8
christian chacon <christian_kazuma@hotmail.com>
2022-02-09 09:44:39 -0500

    Se eliminan los imports que ya no se estan usando

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/controller/ClienteRestController.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/controller/ClienteRestController.java
@@ -1,15 +1,10 @@
 package com.bolsadeideas.springboot.backend.apirest.controller;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
@@ -18,11 +13,8 @@ import com.bolsadeideas.springboot.backend.apirest.models.entities.Cliente;
 import com.bolsadeideas.springboot.backend.apirest.models.service.IClienteService;
 import com.bolsadeideas.springboot.backend.apirest.models.service.IUploadFileService;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -35,13 +27,13 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @CrossOrigin(origins = { "http://localhost:4200" })
 @RestController
@@ -53,8 +45,6 @@ public class ClienteRestController {
     
     @Autowired
     private IUploadFileService uploadFileService;
-
-    private final Logger log = LoggerFactory.getLogger(ClienteRestController.class);
 
     @GetMapping("/clientes")
     public List<Cliente> index() {
